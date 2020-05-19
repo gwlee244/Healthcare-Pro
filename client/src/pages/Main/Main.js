5c4e20d75055dc41763c1daba9f5e9be1c7ded3b
Alteredskull <johnson.diantai@gmail.com>
2020-05-19 18:43:38 -0400

    Sidebar/Sidenav
    Sidebar/Sidenav

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -7,6 +7,8 @@ import { Card } from "../../components/Card";
 import { Input, TextArea, FormBtn } from "../../components/Form";
 import DeleteBtn from "../../components/DeleteBtn";
 import API from "../../utils/API";
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 
 function Main() {
   // Setting our component's initial state
@@ -62,6 +64,65 @@ function Main() {
 
   return (
     <Container fluid>
+      <SideNav
+    onSelect={(selected) => {
+        // Add your code here
+    }}
+>
+    <SideNav.Toggle />
+    <SideNav.Nav defaultSelected="home">
+        <NavItem eventKey="home">
+            <NavIcon>
+                <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                Home
+            </NavText>
+        </NavItem>
+        <NavItem eventKey="log">
+            <NavIcon>
+                <i className="fas fa-user-nurse" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                Doctor
+            </NavText>
+        </NavItem>
+        <NavItem eventKey="clinic">
+            <NavIcon>
+                <i className="fas fa-clinic-medical" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                Clinic
+            </NavText>
+        </NavItem>
+        <NavItem eventKey="chart">
+            <NavIcon>
+                <i className="fas fa-chart-bar" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                Chart
+            </NavText>
+        </NavItem>
+        <NavItem eventKey="healthlog">
+            <NavIcon>
+                <i className="fas fa-heartbeat" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                Health Log
+            </NavText>
+            <NavItem eventKey="healthlog/symptoms">
+                <NavText>
+                Symptoms
+                </NavText>
+            </NavItem>
+            <NavItem eventKey="healthlog/Prescriptions">
+                <NavText>
+                Prescriptions
+                </NavText>
+            </NavItem>
+        </NavItem>
+    </SideNav.Nav>
+</SideNav>
       <Row>
       <Col size="md-9">
           <Card title="Main">
