5bbd421548c85b344bdf1a0c3de9808fff99f978
Gene Lee <gwlee244@gmail.com>
2020-05-24 23:49:54 -0400

    finished validation for login and registration

--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -1,1 +1,15 @@
+import { GET_ERRORS, CLEAR_ERRORS } from "../actions/constants";
+const initialState = {};
 
+export default function(state = initialState, action) {     
+    switch (action.type) { 
+        case GET_ERRORS:      
+            return action.data;
+
+        case CLEAR_ERRORS:             
+            return {};
+
+        default: 
+         return state;
+        }
+}
