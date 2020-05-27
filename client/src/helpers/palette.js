2a75e80a9f772bcb5ee47db241e6b5a434ca3b14
keshavsavva <keshav.s.avva@gmail.com>
2020-05-27 17:45:18 -0400

    added palette.js

---
+++ client/src/helpers/palette.js
@@ -0,0 +1,78 @@
+export const colors = {
+    red: {
+        bgc: "#F44336", /* red */
+        color: "#000"
+    },
+    pink: {
+        bgc: "#E91E63", /* pink */
+        color: "#000"
+    },
+    purple: {
+        bgc: "#9C27B0", /* purple */
+        color: "#000"
+    },
+    deepPurple: {
+        bgc: "#673AB7", /* deepPurple */
+        color: "#000"
+    },
+    indigo: {
+        bgc: "#3F51B5", /* indigo */
+        color: "#000"
+    },
+    blue: {
+        bgc: "#2196F3", /* blue */
+        color: "#000"
+    },
+    lightBlue: {
+        bgc: "#03A9F4", /* lightBlue */
+        color: "#000"
+    },
+    cyan: {
+        bgc: "#00BCD4", /* cyan */
+        color: "#000"
+    },
+    teal: {
+        bgc: "#009688", /* teal */
+        color: "#000"
+    },
+    green: {
+        bgc: "#4CAF50", /* green */
+        color: "#000"
+    },
+    lightGreen: {
+        bgc: "#8BC34A", /* lightGreen */
+        color: "#000"
+    },
+    lime: {
+        bgc: "#CDDC39", /* lime */
+        color: "#000"
+    },
+    yellow: {
+        bgc: "#FFEB3B", /* yellow */
+        color: "#000"
+    },
+    amber: {
+        bgc: "#FFC107", /* amber */
+        color: "#000"
+    },
+    orange: {
+        bgc: "#FF9800", /* orange */
+        color: "#000"
+    },
+    deepOrange: {
+        bgc: "#FF5722", /* deepOrange */
+        color: "#000"
+    },
+    brown: {
+        bgc: "#795548", /* brown */
+        color: "#000"
+    },
+    grey: {
+        bgc: "#9E9E9E", /* grey */
+        color: "#000"
+    },
+    blueGrey: {
+        bgc: "#607D8B", //blueGrey
+        color: "#000"
+    }
+}
