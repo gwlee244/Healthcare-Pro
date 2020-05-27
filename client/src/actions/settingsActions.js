b2eee9e977460d4abaf61d120c00f839dd28d999
Alex Coulter <ALEXCOULTER.UNC@GMAIL.COM>
2020-05-26 20:29:19 -0400

    fixed a few bugs in calendar and settings

--- client/src/actions/settingsActions.js
+++ client/src/actions/settingsActions.js
@@ -1,24 +1,38 @@
-import  { SET_SETTINGS } from "../constants";
+import  { SET_SETTINGS, GET_SETTINGS } from "../constants";
 import axios from "axios";
 
-export const createPatientSettings = (user, settings) => dispatch => {
-    dispatch({type: SET_SETTINGS});
+export const updatePatientSettings = (user, settings) => dispatch => {
     axios
-        .post(`/api/patients/updateSettings/`, {settings, user})
+      .post("/api/patients/updateSettings", {settings, user})
         .then(res => {
             console.log(res)
-            dispatch({type: SET_SETTINGS, data: res.data})
         })
         .catch(err => console.log(err));
 }
 
-export const createDoctorSettings = (user, settings) => dispatch => {
-    dispatch({type: SET_SETTINGS});
+export const updateDoctorSettings = (user, settings) => dispatch => {
     axios
-        .post(`/api/doctors/updateSettings/`, {settings, user})
+        .post("/api/doctors/updateSettings", {settings, user})
         .then(res => {
             console.log(res)
-            dispatch({type: SET_SETTINGS, data: res.data})
         })
         .catch(err => console.log(err));
+}
+
+export const getPatientSettings = patientID => dispatch => {
+  axios
+    .get(`/api/patients/getSettings/${patientID}`)
+      .then(res => {
+          dispatch({type: GET_SETTINGS, data: res.data})
+      })
+      .catch(err => console.log(err));
+}
+
+export const getDoctorSettings = doctorID => dispatch => {
+  axios
+    .get(`/api/patients/getSettings/${doctorID}`)
+      .then(res => {
+          dispatch({type: GET_SETTINGS, data: res.data})
+      })
+      .catch(err => console.log(err));
 }
