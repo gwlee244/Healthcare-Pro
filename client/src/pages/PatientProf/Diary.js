b9a586780106a5a066132ee74398512723596957
Gene Lee <gwlee244@gmail.com>
2020-05-31 22:16:03 -0400

    added imports for new file Diary.js in PatientProf

---
+++ client/src/pages/PatientProf/Diary.js
@@ -0,0 +1,23 @@
+/*
+  Component in Patient Profile that show history of all visits to the doctor, threatment and so on.
+  @imported in PatientProfileTabs
+*/
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableFooter from "@material-ui/core/TableFooter";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import IconButton from "@material-ui/core/IconButton";
+import FirstPageIcon from "@material-ui/icons/FirstPage";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+import LastPageIcon from "@material-ui/icons/LastPage";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import TableHead from "@material-ui/core/TableHead";
+import { connect } from "react-redux";
