163258fe34dfa01cce0776f7473200a4dbeb1c31
ARauf Git <umiasad2017@gmail.com>
2020-06-03 07:27:01 -0400

    fixed bugs

--- client/src/actions/calendarAction.js
+++ client/src/actions/calendarAction.js
@@ -1,30 +1,41 @@
-import  { GET_APPOINTMENTS, APPOINTMENT_ADD } from "./constants";
+import { APPOINTMENT_ADD, GET_APPOINTMENTS } from "./constants";
 import axios from "axios";
 
-export const getPatientAppointments = patientID => dispatch => {
-    dispatch({ type: GET_APPOINTMENTS });
-    axios
-        .get(`/api/patients/appointments/${patientID}`)
-        .then(res => {
-            dispatch({ type: GET_APPOINTMENTS, data: res.data });
-        })
-        .catch(err => console.log(err));
-};
-
+// Get doctor appointments
 export const getDoctorAppointments = doctorID => dispatch => {
-    dispatch({ type: GET_APPOINTMENTS });
-    axios
-        .get(`/api/doctors/appointments/${doctorID}`)
-        .then(res => {
-            dispatch({ type: GET_APPOINTMENTS, data: res.data });
-        })
-        .catch(err => console.log(err));
+	axios
+		.get(`/api/doctors/appointments/${doctorID}`)
+		.then(res => {
+			dispatch({ type: GET_APPOINTMENTS, data: res.data });
+		})
+		.catch(err => console.log(err));
 };
 
-export const appointmentAdd = (doctorID, patientID, appointment, day) => dispatch => {
-    axios
-        .post("/api/doctors/appointments/add", {doctorID, patientID, appointment, day})
-        .then(res => console.log(res))
-        .catch(err => console.log(err))
-        dispatch({ type: APPOINTMENT_ADD, data: appointment, day });
-}
+// Get patient appointments
+export const getPatientAppointments = patientID => dispatch => {
+	axios
+		.get(`/api/patients/appointments/${patientID}`)
+		.then(res => {
+			dispatch({ type: GET_APPOINTMENTS, data: res.data });
+		})
+		.catch(err => console.log(err));
+};
+
+// Add appointment to db
+export const appointmentAdd = (
+	appointment,
+	day,
+	doctorID,
+	patientID
+) => dispatch => {
+	axios
+		.post("/api/doctors/appointments/add", {
+			doctorID,
+			patientID,
+			appointment,
+			day
+		})
+		.then(res => console.log(res))
+		.catch(err => console.log(err));
+	dispatch({ type: APPOINTMENT_ADD, data: appointment, day });
+};
