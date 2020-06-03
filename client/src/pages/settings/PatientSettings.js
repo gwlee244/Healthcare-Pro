3f76d67f44f5bfea8c61b96b99df4e5899a4d8a6
Alteredskull <johnson.diantai@gmail.com>
2020-06-03 05:35:46 -0400

    Update PatientSettings.js
    Added PatientSettings Class

--- client/src/pages/settings/PatientSettings.js
+++ client/src/pages/settings/PatientSettings.js
@@ -62,3 +62,93 @@
 // 		justifyContent: "space-between"
 // 	}
 // });
+
+// class PatientSettings extends React.Component {
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = {
+// 			expanded: null,
+// 			openSnackBar: false,
+// 			address: {
+// 				street: "",
+// 				city: "",
+// 				number: ""
+// 			},
+// 			birthday: "",
+// 			sex: "",
+// 			email: "",
+// 			work: "",
+// 			maritalStatus: "",
+// 			emergency: {
+// 				fName: "",
+// 				lName: "",
+// 				relation: "",
+// 				phoneNumber: ""
+// 			},
+// 			phone: "",
+// 			allergies: "",
+// 			medAllergies: "",
+// 			injuries: "",
+// 			operations: "",
+// 			currMeds: "",
+// 			height: "",
+// 			weight: "",
+// 			blood: {
+// 				type: "",
+// 				rhesus: ""
+// 			}
+// 		};
+// 		this.handleExpand = this.handleExpand.bind(this);
+// 		this.handleCloseSnackBar = this.handleCloseSnackBar.bind(this);
+// 		this.onAddress = this.onAddress.bind(this);
+// 		this.onEmergency = this.onEmergency.bind(this);
+// 		this.onChangeSettings = this.onChangeSettings.bind(this);
+// 		this.onChangeBloodType = this.onChangeBloodType.bind(this);
+// 		this.onSave = this.onSave.bind(this);
+// 	}
+// 	handleExpand = panel => (event, expanded) => {
+// 		this.setState({
+// 			expanded: expanded ? panel : false
+// 		});
+// 	};
+// 	handleCloseSnackBar = () => {
+// 		this.setState({ openSnackBar: false });
+// 	};
+// 	onAddress = ev => {
+// 		this.setState({
+// 			address: Object.assign({}, this.state.address, {
+// 				[ev.target.name]: ev.target.value
+// 			})
+// 		});
+// 	};
+// 	onEmergency = ev => {
+// 		this.setState({
+// 			emergency: Object.assign({}, this.state.emergency, {
+// 				[ev.target.name]: ev.target.value
+// 			})
+// 		});
+// 	};
+// 	onChangeSettings = ev => {
+// 		this.setState({
+// 			[ev.target.name]: ev.target.value
+// 		});
+// 	};
+// 	onChangeBloodType = ev => {
+// 		this.setState({
+// 			blood: Object.assign({}, this.state.blood, {
+// 				[ev.target.name]: ev.target.value
+// 			})
+// 		});
+// 	};
+// 	componentDidMount = () => {
+// 		this.props.getPatientSettings(this.props.auth.user.id);
+// 	};
+// 	componentWillReceiveProps(nextProps) {
+// 		if (nextProps.settings) {
+// 			this.setState(Object.assign({}, nextProps.settings));
+// 		}
+// 	}
+// 	onSave = ev => {
+// 		this.setState({ openSnackBar: true, expanded: null });
+// 		this.props.updatePatientSettings(this.state, this.props.auth.user.id);
+//     };
